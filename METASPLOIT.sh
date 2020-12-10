505ea9e2beacb55c51fc1e71680f372e0d75c0d0
Termux <70668507+TermuxHackz@users.noreply.github.com>
2020-12-10 13:08:53 +0100

    Add files via upload

---
+++ METASPLOIT.sh
@@ -0,0 +1,79 @@
+#!/termux/data/data/bin/bash
+# script by AnonyminHack5 from [ TERMUX ANDROID HACKERS ] youtube channel
+apt-get update -y
+apt-get upgrade -y
+pkg install python2 -y
+pkg install python -y
+pip install lolcat
+clear
+echo $red 
+echo ' 
+ _______  _______  _______  _______  _______  _______  _______ _________ _        _______   
+(  ____ )(  ____ )(  ___  )(  ____ \(  ____ \(  ____ \(  ____ \\__   __/( (    /|(  ____ \  
+| (    )|| (    )|| (   ) || (    \/| (    \/| (    \/| (    \/   ) (   |  \  ( || (    \/  
+| (____)|| (____)|| |   | || |      | (__    | (_____ | (_____    | |   |   \ | || |        
+|  _____)|     __)| |   | || |      |  __)   (_____  )(_____  )   | |   | (\ \) || | ____   
+| (      | (\ (   | |   | || |      | (            ) |      ) |   | |   | | \   || | \_  )  
+| )      | ) \ \__| (___) || (____/\| (____/\/\____) |/\____) |___) (___| )  \  || (___) |  
+|/       |/   \__/(_______)(_______/(_______/\_______)\_______)\_______/|/    )_)(_______)  
+                                                                                            
+
+/....TuRn On yOur IntErneT coNnecTion....\' | lolcat
+echo " "
+echo "### This Script is written by AnonyminHack5 ###" 
+echo " "
+echo "----------- Fetching.../// ---------️----"
+echo " "
+pkg install wget -y
+pkg install openssh -y
+pkg install ruby -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+
+ls
+
+cd ..
+
+ls
+
+cd usr
+
+ls
+
+cd opt
+
+ls
+
+mv metasploit-framework $HOME
+
+ls
+
+cd /data/data/com.termux/files/home
+
+ls
+
+cd metasploit-framework
+
+ls
+
+wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt
+
+bash fix-ruby-bigdecimal.sh.txt
+
+echo " "
+echo "############# Completed ################"                                                                                                                       
+echo " "
+echo "If You Are in Internet Then 
+you have downloaded metasploit successfully. 
+Subscribe us on youtube [ CYBER SEC ]"
+echo " "
+echo " (x ...BE A WHITE HAT HACKER... x) "
+echo " "
+echo '
+echo " "
+echo "Type msfconsole to start ./msfconsole..."
+echo " "
+
+./msfconsole
+
+exit
